8612513cf6dd1c5250377910d8b6789ae49bed8b
Arya <aryaashari100@gmail.com>
2023-01-30 15:30:09 +0700

    add env config file

--- app/server.go
+++ app/server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -14,19 +16,41 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) Initialize() {
-	fmt.Println("E-Commerce")
+type Config struct {
+	appName, appEnv, appPort string
+}
+
+func (server *Server) Initialize(config Config) {
+	fmt.Println(config.appName)
 	server.Router = mux.NewRouter()
 	server.InitRouter()
 }
 
-func (server *Server) Run(addr string) {
-	fmt.Println("Running Server On Port", addr)
-	log.Fatal(http.ListenAndServe(addr, server.Router))
+func (server *Server) Run(config Config) {
+	fmt.Println("Running", config.appEnv, "Server On Port", config.appPort)
+	log.Fatal(http.ListenAndServe(config.appPort, server.Router))
+}
+
+func getEnv(key, defaultName string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return defaultName
 }
 
 func Run() {
 	server := Server{}
-	server.Initialize()
-	server.Run(":2023")
+	config := Config{}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	config.appName = getEnv("APP_NAME", "E-Commerce")
+	config.appEnv = getEnv("APP_ENV", "dev")
+	config.appPort = getEnv("APP_PORT", ":2023")
+
+	server.Initialize(config)
+	server.Run(config)
 }
